810ae78525cd6544a4d44a3899f738b85b0bfa32
duhengqi <1292071940@qq.com>
2023-04-18 12:32:56 +0800

    feats: 格式修改

--- src/learncpp.cpp
+++ src/learncpp.cpp
@@ -1,46 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <list>
-#include <stack>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
 #include <algorithm>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
+#include <vector>
 
 using namespace std;
-/* 
-题目：451. 根据字符出现频率排序
-链接：https://leetcode.cn/problems/sort-characters-by-frequency/
-*/
 
+/*
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+如果数组中不存在目标值 target，返回 [-1, -1]。
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+示例 1：
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+*/
 class Solution {
 public:
-    string frequencySort(string s) {
-        string result;
-        map<char, int> freqMap;
-        for (size_t i = 0; i < s.length(); i++) {
-            freqMap[s[i]]++;
-        }
-        auto iter = freqMap.begin();
-        for (; iter != freqMap.end(); iter++) {
-            for (int i = 0; i < iter->second; i++) {
-                result += iter->first;
+    vector<int> searchRange(vector<int>& nums, int target)
+    {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                result.push_back(i);
             }
         }
-        cout << result << endl;
-        reverse(result.begin(), result.end());
-        cout << result << endl;
         return result;
     }
+
 };
+
 int main()
 {
-    int a = 5;
     Solution so;
-    so.frequencySort("tree");
-    int b = a + 3;
+
     system("pause");
     return 0;
 }
